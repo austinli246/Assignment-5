61e0e7d0f4a7cfbc03e816aa6c384a3d607d4f2a
Austin Li <liaustin926@gmail.com>
2021-01-09 17:00:50 -0500

    added file remove row and column

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //add rows and
 const submit = document.getElementById('submit')
 submit.onclick = function(){
-    let rows = document.getElementById('rows').value;
-    let columns = document.getElementById('columns').value;
+    const rows = document.getElementById('rows').value;
+    const columns = document.getElementById('columns').value;
     const nostuff = document.getElementById('container1')
     if (rows == "" || rows == null || columns == "" || columns == null){
         let txt = document.createElement('a')
         txt.innerHTML = "ENTER ROWS/COLUMNS"
         nostuff.append(txt)
     }
-    let output ='<table>'      
+    let output ='<table id ="table">'      
         function createTable(rows,columns){
             for(var i=1;i<=rows;i++){
                 output=output+'<tr>'
@@ -19,8 +19,44 @@ submit.onclick = function(){
                 output=output+'</tr>'
             }
             output=output+'</table>'
-            document.getElementById('container2').innerHTML=output    
+            document.getElementById('container1').innerHTML=output    
        }
-       createTable(rows,columns)
+    createTable(rows,columns)
+    
+ 
+}
+
+
+
+const Delete = document.getElementById('delete')
+
+Delete.addEventListener('click', deleteTable)
+function deleteTable(){
+    const numrowsdel = document.getElementById('Row').value
+    const numcolumnsdel = document.getElementById('Column').value
+    const deletesection = document.querySelector('tbody')
+   
+ 
+    for (let i =0; i<numrowsdel; i++){
+            const delrow = document.querySelector('tr')
+            delrow.remove()
     }
+    console.log(deletesection)
+ 
+    let tables = document.getElementById('table'), rows = table.rows;
+
+    if (numcolumnsdel!=0){
+        for (let i=0; i<rows[0].cells.length; i++){
+            for (let j =0; j <rows.length; j++){
+                rows[j].deleteCell(i)
+            }
+        }
+    }
+  
+}
+
+
+
+
+
 
