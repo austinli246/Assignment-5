fcf2619564f8cec5015d87ac6041e4aba778ec45
Austin Li <liaustin926@gmail.com>
2021-01-11 17:48:27 -0500

    added clicked on cell (color)

--- script.js
+++ script.js
@@ -4,31 +4,46 @@ submit.onclick = function(){
     const rows = document.getElementById('rows').value;
     const columns = document.getElementById('columns').value;
     const nostuff = document.getElementById('container1')
+    const main = document.getElementById('grid')
+   
     if (rows == "" || rows == null || columns == "" || columns == null){
         let txt = document.createElement('a')
         txt.innerHTML = "ENTER ROWS/COLUMNS"
         nostuff.append(txt)
     }
-    let output ='<table id ="table">'      
+    let output ='<table id ="table" border = "1">'      
         function createTable(rows,columns){
             for(var i=1;i<=rows;i++){
-                output=output+'<tr>'
+                var newrow = document.createElement('tr');
+         
+               // output=output+'<tr>'
                 for(var j=1;j<=columns;j++){
-                    output=output+'<td>'+'<input placeholder=" ">'+'</td>'
+                    let newCell = document.createElement('td');
+                   // output=output+'<td>'+'<input placeholder="0" value = "0">'+'</td>'
+                   newrow.appendChild(newCell);
+                   addGridFunction(newCell);
                 }
-                output=output+'</tr>'
+               
+                //output=output+'</tr>'
+                main.appendChild(newrow)
             }
-            output=output+'</table>'
-            document.getElementById('container1').innerHTML=output    
+            //output=output+'</table>'
+            //document.getElementById('container1').innerHTML=output    
+           
        }
     createTable(rows,columns)
-    
- 
+  
 }
+
+// for (let i =0; i<rows*columns; i++){
+//     const newCell = document.getElementsByTagName('td')
+//     addGridFunction(newCell)
+//     }
 
 
 
 const Delete = document.getElementById('delete')
+
 
 Delete.addEventListener('click', deleteTable)
 function deleteTable(){
@@ -41,19 +56,50 @@ function deleteTable(){
             const delrow = document.querySelector('tr')
             delrow.remove()
     }
-    console.log(deletesection)
  
-    let tables = document.getElementById('table'), rows = table.rows;
+    let tables = document.getElementById('grid')
+    rows = tables.rows;
 
-    if (numcolumnsdel!=0){
-        for (let i=0; i<rows[0].cells.length; i++){
-            for (let j =0; j <rows.length; j++){
-                rows[j].deleteCell(i)
+    let totalColumns = rows[0].cells.length; //TOTAL NUMBER OF COLUMNS
+    if (numcolumnsdel > totalColumns) {
+        //this condition checks if the users is atempting to delete more columns than what exists
+        return alert(`You only have ${totalColumns} rows`)
+    }
+    if (numcolumnsdel != 0) {
+        for (let i = 0; i < rows.length; i++) {
+            let currRow = rows[i];
+            for (let j = 0; j < numcolumnsdel; j++) {
+                let cells = currRow.cells;
+                let currCell = cells[j]
+                currRow.deleteCell(currCell)
             }
         }
     }
-  
+
 }
+
+
+
+
+//adding functionality 
+function addGridFunction(cell){
+    //change color when clicked
+    cell.addEventListener("click", setBackgroundColor)
+
+
+
+}
+let currColor = document.getElementById('colorOption').value
+function setCurrColor(color) {
+    currColor = color; 
+    console.log(currColor)
+
+}
+
+function setBackgroundColor(value){
+    this.style.backgroundColor = currColor;
+}
+
 
 
 
